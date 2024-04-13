37c9352a8525f05d056860cabacfdac9c4524e5e
Endyths <endythsxd@gmail.com>
2024-04-12 22:58:11 -0400

    Curriculum

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Alumno' ,
+  res.render('index', { title: 'CV' ,
 nombre: "Endyths",
-apellido: "Carrasquel",
+correo: "endythsxd@gmail.com",
 Cedula: "32008046",});
 });
 
