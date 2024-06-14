6d49577b5b6fd0973d2d1c56e26f74152cf1a790
endyths <endythsxd@gmail.com>
2024-06-14 07:59:49 -0400

    volvemos a probar

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/AuthController');
 const contactosController = require('../controllers/ContactosController');
 const contactoControllers= new contactosController()
 
